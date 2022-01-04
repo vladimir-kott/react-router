3b72878d09d27feb78a8eb75c655eb35d564631a
Vladimir Kott <vladimirkott19@gmail.com>
2022-01-04 18:58:03 +0200

    11

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,10 @@
 import NavBar from "./components/navBar"
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import Dashboard from '../app/components/dashboard'
 import Login from '../app/components/login'
 import Posts from '../app/components/posts'
 import Home from '../app/components/home'
+import NotFound from "./components/not-found"
 
 function App() {
   return (
@@ -11,13 +12,15 @@ function App() {
       <NavBar/>
       <h1>App</h1>
       <Switch>
+        <Route path='/' exact component={Home}/>
         <Route 
           path='/dashboard' 
           component={Dashboard}
         />
         <Route path='/login' component={Login}/>
         <Route path='/posts/:postId?' component={Posts}/>
-        <Route path='/' exact component={Home}/>
+        <Route path='/404' component={NotFound}/>
+        <Redirect to="/404"/>
       </Switch>
     </div>
   );
--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,17 +1,23 @@
 import PostsList from "./postsList"
 import Post from "./post"
+import query from "query-string"
+import _ from "lodash"
 
-const Posts = ({match}) => {
+const Posts = ({match, location}) => {
     const posts = [
         {id: 1, label: 'post 1'},
         {id: 2, label: 'post 2'},
         {id: 3, label: 'post 3'}
     ]
 
+    const search = query.parse(location.search)
+    console.log(search)
     const postId = match.params.postId
 
+    const cropPosts =search? _(posts).slice(0).take(search.count).value():posts
+
     return <>
-        {postId?<Post posts={posts} id = {postId}/>:<PostsList posts={posts}/>}
+        {postId?<Post posts={posts} id = {postId}/>:<PostsList posts={cropPosts}/>}
     </>
 }
  
